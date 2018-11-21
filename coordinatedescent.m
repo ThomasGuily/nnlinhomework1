427aaae7d8c894990f86afee9296220c469ff72d
Guily <thomas.guily1998@gmail.com>
2018-11-21 17:44:22 +0100

    coordinate descent is currently working

--- coordinatedescent.m
+++ coordinatedescent.m
@@ -10,11 +10,26 @@ function x=coordinatedescent(Q,c,p,x0,maxiter)
   % x est vecteur n x 1
   
   % ICI :Algorithme à implémenter
-  a = size x0;
+  a = size (c);
   n = a(2);
-  
+  A =0;
+ 
   for i=1:maxiter
+    [f] = fcalculator (Q, x0, c, p)
+    z(i) = f ; 
     j = ceil(rand(1)*n) ; 
-    x(j) = 
-  end
-end
+    for o = 1:n
+      A -= Q(j,o)*x0(o) + Q(o,j)*x0(o); 
+    endfor
+    x0(j) = (A+ (2* Q(j,j)) + c(j))/2*Q(j,j)
+    
+    %if i>1
+      %if z(i) <= z(i-1)
+        %break;
+      %endif
+    %endif
+    
+  endfor
+  fprintf('La valeur optimale de x est %f\n', x0);
+  fprintf('La valeur optimale de la fonction est %f\n', f);
+endfunction
