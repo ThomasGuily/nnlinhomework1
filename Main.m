a48b5f5fabac746f3071c919979dc72c5cf6f21a
Guily <thomas.guily1998@gmail.com>
2018-11-21 17:43:09 +0100

    this programm can solve any optimisation problem based on a quadratic function

--- Main.m
+++ Main.m
@@ -1,13 +1,13 @@
 %Main
 
 choix = input ('Choix : 0 = problème spécifique ; 1 = problème random ; 2 = Tapez votre propre problème ==>');
-
+n=0;
 if (choix == 0)
   
   [Q, x0, c, p] = quadfunctiongenerator(n,choix)
-  [f] = fcalculator (Q, x0, c, p)
-  maxiter = 1000 ; 
-  %x = coordinatedescent(Q,c,p,x0,maxiter);
+  
+  maxiter = input ( 'Nbre itérations :'); 
+  x = coordinatedescent(Q,c,p,x0,maxiter);
   
   
 else
