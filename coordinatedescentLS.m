4f3c8a9b0ae1d2990c68d79f2a054eec61092ba1
gmagannaDevelop <gmaganna.biomed@gmail.com>
2018-11-26 19:05:13 +0100

    Included coordinate descent for least squares. Corrected a typo on the PDF.

--- coordinatedescentLS.m
+++ coordinatedescentLS.m
@@ -1,4 +1,4 @@
-function [Q, c, p] = coordinatedescentLS(A, b, x0, maxiter)
+function [x, z] = coordinatedescentLS(A, b, x0, maxiter)
   % Entrees :
   % A est une matrice m x n
   % b est un vecteur m x 1s
@@ -11,8 +11,10 @@ function [Q, c, p] = coordinatedescentLS(A, b, x0, maxiter)
   % ICI :Algorithme a implementer
   
   % A partir de notre deduction:
+  Q = (A')*A;
+  c = (b')*A;
+  p = (b')*b;
   
-  
- 
+  [x, z] = coordinatedescent(Q, c, p, x0, maxiter);
 
 end
