a55ba8de6a355922fdb783b96947576530687077
Guily <thomas.guily1998@gmail.com>
2018-11-27 21:23:24 +0100

    partie du code cloturée et commentée

--- convergencespeed.m
+++ convergencespeed.m
@@ -1,35 +1,19 @@
-## Copyright (C) 2018 ThomasGuily
-## 
-## This program is free software: you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see
-## <https://www.gnu.org/licenses/>.
 
-## -*- texinfo -*- 
-## @deftypefn {} {@var{retval} =} convergencespeed (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
 
-## Author: ThomasGuily <ThomasGuily@THOMASGUILY-PC>
+## Author: ThomasGuily, Gustavo Magaña Lopez
 ## Created: 2018-11-26
 
 function convergencespeed (x, z)
-  for i = 1 : size(x)(1)-1
-    
+  for i = 1 : size(x)(1)-1    
     C(i) =  ((((x(i+1,1) - x(size(x)(1),1))^2 + (x(i+1,2) - x(size(x)(1),2))^2))/(((x(i,1) - x(size(x)(1),1))^2 + (x(i,2) - x(size(x)(1),2))^2)))^0.5;
   endfor
-  m = linspace(1, size(x)(1)-1,size(x)(1)-1);
-  figure 
+  %calcule selon la formule la vitesse de convergence les valeurs de celle ci à chaques itérées
+  
+  m = linspace(1, size(x)(1)-1,size(x)(1)-1); 
+  %crée un vecteur représentant le nombre d'itérations
+  
+  figure
+  %crée une nouvelle figure afin de ne pas cacher la visualisation en 3D 
   plot(m, C,'r');
   hold on;
 endfunction
--- coordinatedescentLS.m
+++ coordinatedescentLS.m
@@ -11,10 +11,8 @@ function [x, z] = coordinatedescentLS(A, b, x0, maxiter)
   % ICI :Algorithme a implementer
   
   % A partir de notre deduction:
-  Q = (A')*A;
-  c = (b')*A;
-  p = (b')*b;
   
-  [x, z] = coordinatedescent(Q, c, p, x0, maxiter);
-
+  %La fonction coordinatedescentLS n'est pas utilisée car sa version 
+  %(coordinatedescent) générale marche très bien à partir du moment ou
+  % Q est définie positive
 end
